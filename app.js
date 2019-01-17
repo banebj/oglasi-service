f5b22bc4b4a6e9644f9c9d7d4ca1b5c934a7dec6
Branislav Jovanovic <branislav.jovanovic@devoteam.com>
2019-01-17 13:19:45 +0100

    bugfix import

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const oglasiService = require('./services/oglasi.service');
 let CronJob = require('cron').CronJob;
 
